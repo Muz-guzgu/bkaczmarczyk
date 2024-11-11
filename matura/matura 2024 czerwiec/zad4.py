7c271a07dc21da741ba31a94c0d8924e255fb173
Muz-guzgu <batek05ka@gmail.com>
2024-11-11 10:27:27 +0100

    2024_czerwiec, zad 4.1 i 4.4

--- matura/matura 2024 czerwiec/zad4.py
+++ matura/matura 2024 czerwiec/zad4.py
@@ -2,9 +2,20 @@ def wczytaj_dane(nazwa_pliku):
     with open(nazwa_pliku) as plik:
         return [wiersz.strip() for wiersz in plik]
 
-def zapisz_do_pliku(nazwa_pliku, dane):
+def zapisz_dane(nazwa_pliku, dane):
     with open(nazwa_pliku, "w") as plik:
         plik.write(dane)
+
+def zad_4_1(wiersze):
+    licznik = 0
+    for wiersz in wiersze:
+        p = 0
+        while p + 2 < len(wiersz):
+            if wiersz[p] == "k" and wiersz[p+2] == "t":
+                licznik += 1
+                break
+            p += 1
+    return licznik
 
 def zad_4_2(wiersze):
     lista_fragmentow = []
@@ -18,9 +29,58 @@ def zad_4_2(wiersze):
             p += 1
             k += 1
     return lista_fragmentow
-#print(zapisz_do_pliku("zad_4.2", zad_4_2(wczytaj_dane("slowa_przyklad.txt"))))
+
+
+def zad_4_3(wiersze):
+    najdluzszy = ""
+    licznik = 0
+    for wiersz in wiersze:
+        if wiersz[::-1] == rot(wiersz):
+            licznik += 1
+            if len(wiersz) > len(najdluzszy):
+                najdluzszy = wiersz
+    return str(licznik) + " " + najdluzszy
+def rot(wiersz):
+    slowo = ""
+    for litera in wiersz:
+        ascii_litery = ord(litera)
+        if ascii_litery < 110:
+            ascii_litery += 13
+        else:
+            ascii_litery -= 13
+        slowo = slowo + chr(ascii_litery)
+    return slowo
+
+
+def zad_4_4(wiersze):
+    lista_slow = []
+    for wiersz in wiersze:
+        wiersz_sorted = "".join(sorted(wiersz))
+        if licznik_największej(wiersz_sorted) >= len(wiersz)/2:
+            lista_slow.append(wiersz)
+    return lista_slow
+
+def licznik_największej(slowo):
+    licznik = 0
+    max_licznik = 0
+    for index in range(len(slowo)):
+        if index == 0:
+            licznik += 1
+        if slowo[index] == slowo[index - 1]:
+            licznik += 1
+        else:
+            licznik = 1
+        max_licznik = max(licznik, max_licznik)
+    return max_licznik
+
 
 if __name__ == "__main__":
     dane = wczytaj_dane("slowa.txt")
     dane_zad_4_2 = " ".join(zad_4_2(dane))
-    zapisz_do_pliku("zad_4.2", dane_zad_4_2)
+    dane_zad_4_1 = zad_4_1(dane)
+    dane_zad_4_3 = zad_4_3(dane)
+    dane_zad_4_4 = " ".join(zad_4_4(dane))
+    print(dane_zad_4_4)
+    dane_do_zapisu = f"4.1 {dane_zad_4_1}\n4.2 {dane_zad_4_2}\n4.3 {dane_zad_4_3}\n4.4 {dane_zad_4_4}"
+    zapisz_dane("zad_4", dane_do_zapisu)
+
